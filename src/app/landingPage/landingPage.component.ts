f93111dc78356532e791305b5dc9f530b5fc55d9
Ray <rainer.musch@gmx.de>
2025-02-11 15:51:16 +0100

    add navbar

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,12 +1,20 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { NavbarComponent }  from './navbar/navbar.component';
 
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [CommonModule],
-    template: '<h1>Hello World</h1>',
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section>
+        <app-navbar> </app-navbar>
+        <h1 class="fontSixtyfour">Rays first Angular Projekt</h1>
+        <h2>Hello World</h2>
+    </section>
+    
+    `,
     styleUrls: ['./landingPage.component.scss']
 })
 export class LandingPageComponent {}

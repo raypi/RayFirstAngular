313e6cbdaa1c5d0678faa930c36a334537a72979
Ray <rainer.musch@gmx.de>
2025-02-11 13:11:32 +0100

    add landingPage.components.ts

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 
 @Component({
     selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule],
+    template: '<h1>Hello World</h1>',
+    styles: ['h1 {color: green;}']
 })
 export class LandingPageComponent {}
